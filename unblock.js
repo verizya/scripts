9c9985c798a6cb91979cd0fefb8f82a6e3ca5fec
verizya <109697682+verizya@users.noreply.github.com>
2022-12-16 08:45:00 -0500

    test if this works better

--- unblock.js
+++ unblock.js
@@ -2,24 +2,13 @@
 // i could make this more readable or efficent but im not a javascript dev
 
 let url = prompt("Enter URL:");
+fetch(url)
+.then(response => response.text())
+.then(text => let contents = text);
 
 let page = window.open();
 page.document.open();
-page.document.write(`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-<title>Classes</title>
-</head>
-<body>
-  <<iframe id="embed"></iframe>
-</body>
-</html>`);
-
-let iframe = page.document.getElementById('embed');
-iframe.width = "1280";
-iframe.height = "720";
-iframe.src = url;
+page.document.write(contents);
 
 /*
 bookmarklet
